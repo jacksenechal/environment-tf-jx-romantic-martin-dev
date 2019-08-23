f833d8c39c131879661f90a9b89ab7bcc6bbbb08
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-08-23 18:51:00 +0200

    fix: unify the scripts which set up and run the bdd to avoid changing the working dir between the steps
    Signed-off-by: Cosmin Cojocar <cosmin.cojocar@gmx.ch>

--- bdd/bdd.sh
+++ bdd/bdd.sh
@@ -2,11 +2,12 @@
 set -e
 set -x
 
-if [ $# -eq 0 ]; then
-    echo "Please provide the configuration path"
+if [ $# -ne 2 ]; then
+    echo "Please provide the source and destination paths for configuration"
     exit -1
 fi
-CONFIG_PATH=$1
+SRC_PATH=$1
+DST_PATH=$2
 
 export GH_USERNAME="jenkins-x-bot-test"
 export GH_EMAIL="jenkins-x@googlegroups.com"
@@ -45,6 +46,13 @@ export JX_VALUE_PROW_HMACTOKEN="$GH_CREDS_PSW"
 # TODO temporary hack until the batch mode in jx is fixed...
 export JX_BATCH_MODE="true"
 
+# prepare the BDD configuration
+mkdir -p $DST_PATH
+cp -r `ls -A | grep -v "${DST_PATH}"` $DST_PATH
+cp $SRC_PATH/jx-requirements.yml $DST_PATH
+cp $SRC_PATH/parameters.yaml $DST_PATH/env
+cd $DST_PATH
+
 # TODO hack until we fix boot to do this too!
 helm init --client-only
 helm repo add jenkins-x https://storage.googleapis.com/chartmuseum.jenkins-x.io
@@ -53,7 +61,7 @@ jx step bdd \
     --use-revision \
     --version-repo-pr \
     --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git \
-    --config $CONFIG_PATH \
+    --config $SRC_PATH/cluster.yaml \
     --gopath /tmp \
     --git-provider=github \
     --git-username $GH_USERNAME \
